a7a9fdc3a0d75564d147a11ea5765dec40bad644
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-07-06 15:25:16 -0400

    Adding more switch cases to 'toRomanNumber' class, and its tests

--- src/RomanCalculator.js
+++ src/RomanCalculator.js
@@ -4,7 +4,21 @@ class RomanCalculator{
     }
     toRomanNumber(numberDecimal){
         switch(numberDecimal){
-            case 5: return 'V';
+            case 'I': return 1;            
+            //case 'IV': return 4;
+            case 'V': return 5;
+            //case 'IX': return 9;
+            case 'X': return 10;
+            //case 'XL': return 40;
+            case 'L': return 50;
+            //case 'XC': return 90;
+            case 'C': return 100;
+            //case 'CD': return 400;
+            case 'D': return 500;
+            //case 'CM': return 900;            
+            case 'M': return 1000;  
+            default: 
+            break;
         }
     }
 }
--- test/RomanCalculator.test.js
+++ test/RomanCalculator.test.js
@@ -1,6 +1,36 @@
 import {RomanCalculator} from '../src/RomanCalculator'
 
+test('Give a number decimal 1 toRomanNumber should return I',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(1)).toBe('I');    
+})
+
 test('Give a number decimal 5 toRomanNumber should return V',()=>{
     const obj = new RomanCalculator();
     expect(obj.toRomanNumber(5)).toBe('V');    
+})
+
+test('Give a number decimal 10 toRomanNumber should return X',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(10)).toBe('X');    
+})
+
+test('Give a number decimal 50 toRomanNumber should return L',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(50)).toBe('L');    
+})
+
+test('Give a number decimal 100 toRomanNumber should return C',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(100)).toBe('C');    
+})
+
+test('Give a number decimal 500 toRomanNumber should return D',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(500)).toBe('D');    
+})
+
+test('Give a number decimal 1000 toRomanNumber should return M',()=>{
+    const obj = new RomanCalculator();
+    expect(obj.toRomanNumber(1000)).toBe('M');    
 })
