998b7ef348495a9daecba8bfb491e21f3d22f6a3
Javier Choque <javier.dreivaj@gmail.com>
2021-07-06 15:06:03 -0400

    Baby step

---
+++ src/RomanCalculator.js
@@ -0,0 +1,12 @@
+class RomanCalculator{
+    constructor(){
+
+    }
+    toRomanNumber(numberDecimal){
+        switch(numberDecimal){
+            case 5: return 'V';
+        }
+    }
+}
+
+export {RomanCalculator}
