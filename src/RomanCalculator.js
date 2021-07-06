9c736c1f504485fba5fc7d57264f362e2bedf4e0
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-07-06 15:48:28 -0400

    Delete comments and functions that are not being used

--- src/RomanCalculator.js
+++ src/RomanCalculator.js
@@ -2,25 +2,6 @@ class RomanCalculator{
     constructor(){
 
     }
-    getRomanNumberValue(romanNumeral){
-        switch(romanNumeral){
-            case 1: return 'I';            
-            //case 'IV': return 4;
-            case 5: return 'V';
-            //case 'IX': return 9;
-            case 10: return 'X';
-            //case 'XL': return 40;
-            case 50: return 'L';
-            //case 'XC': return 90;
-            case 100: return 'C';
-            //case 'CD': return 400;
-            case 500: return 'D';
-            //case 'CM': return 900;            
-            case 1000: return 'M';  
-            default: 
-            break;
-        }
-    }
 
     getDecimalNumberValue(romanNumeral){
         switch(romanNumeral){
